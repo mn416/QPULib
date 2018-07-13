c8695831de58aaed764d887662115194badfe584
Wim Rijnders <wrijnders@gmail.com>
2018-07-13 09:53:06 +0200

    Detect Pi for older versions

--- Tools/detectPlatform.sh
+++ Tools/detectPlatform.sh
@@ -9,6 +9,9 @@
 
 file=/sys/firmware/devicetree/base/model
 
+# This works for later version of Pi (distro's)
+if false; then
+
 if [ -f $file ]
 then
 	model=$(tr -d '\0' < $file)  # as `cat`, but avoid warning 'ignored null byte in input'
@@ -25,6 +28,19 @@ then
 		fi
 	fi
 fi
+
+fi
+
+# This should work for all Pi's
+# The hardware from Pi 2 onward is actually BCM2836, but the cat-call still returns BCM2835
+model=$(cat /proc/cpuinfo | grep Hardware | grep BCM2835 )
+ret=$?
+if [ $ret -eq 0 ]
+then
+	echo model checks out
+	exit 0
+fi
+
 
 echo This is not an RPi platform
 exit 1
